e2de9e6acdb576e5f686fd3d64611210c7442049
Jean-Philippe-Carre <jp.carre@hotmail.fr>
2024-07-02 11:41:58 +0400

    jpcarre

--- Week_5/Day_4/test.py
+++ Week_5/Day_4/test.py
@@ -1,5 +1,11 @@
-from functools import reduce
-my_list = [1, 3, 5, 7]
-reduced_list = reduce(lambda first, second: first+second, my_list)
+# lst = [1, 2, 3, 6, 7, 8]
+# new_lst = [4, 5]
+# position = 3
+# combined_lst = lst[:position] + new_lst + lst[position:]
+# print(combined_lst)
 
-print(reduced_list)
+lst = ['paul', 'rita', 'jean']
+new_lst = ['karl']
+position = 2
+combined_lst = lst[:position] + new_lst + lst[position:]
+print('\n', combined_lst, '\n')
