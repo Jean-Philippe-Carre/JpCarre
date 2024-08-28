ba678f90cf2b88f6b74377945686b04ee10779b7
Jean-Philippe-Carre <jp.carre@hotmail.fr>
2024-08-28 10:54:00 +0400

    new

---
+++ Week_14/Day_3/index.js
@@ -0,0 +1,12 @@
+let age = prompt("Enter your age: ");
+
+if (age == 18) {
+  alert("Congratulations on your first year of driving!");
+  //   console.log("Congratulations on your first year of driving!");
+} else if (age > 18) {
+  alert("Powering on, enjoy the ride!");
+  //   console.log("Powering on, enjoy the ride!");
+} else {
+  alert("Sorry, you are too young to drive this car, Powering off.");
+  //   console.log("Sorry, you are too young to drive this car, Powering off.");
+}
